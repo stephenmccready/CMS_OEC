0a89e24bf8519743104c498742aed7f45c7b0d74
Stephen McCready <stephenmccready@gmail.com>
2014-12-15 10:32:21 -0500

    Create Populate_tbl_SEP_Reason.sql

---
+++ Populate_tbl_SEP_Reason.sql
@@ -0,0 +1,30 @@
+Insert	Into tbl_SEP_Reason Select	'I am new to Medicare.', 'NEW', 1
+Insert	Into tbl_SEP_Reason Select	'I recently moved outside of the service area for my current plan.', 'MOV', 1
+Insert	Into tbl_SEP_Reason Select	'I have both Medicare and Medicaid or my state helps pay for my Medicare Premiums.', 'MDE', 1
+Insert	Into tbl_SEP_Reason Select	'I am moving into, live in, or recently moved out of a Long-Term Care Facility (for example, a nursing home or rehabilitation hospital).', 'LTC', 1
+Insert	Into tbl_SEP_Reason Select	'I recently “left” a Programs of All-inclusive Care for the Elderly program.', 'PAC', 1
+Insert	Into tbl_SEP_Reason Select	'I recently involuntarily lost my creditable drug coverage.', 'LCC', 1
+Insert	Into tbl_SEP_Reason Select	'I am losing creditable drug coverage I had from an employer.', 'LEC', 1
+Insert	Into tbl_SEP_Reason Select	'I belong to a pharmacy assistance program provided by my state, or I am losing or recently lost participation in such a program.', 'PAP', 1
+Insert	Into tbl_SEP_Reason Select	'I recently returned to the United States after living permanently outside of the U.S.', 'RUS', 1
+Insert	Into tbl_SEP_Reason Select	'In the last 12 months, I left a Medigap policy to join a Medicare Advantage Plan* for the first time (*Medicare Advantage plan with prescription drug coverage)', '12G', 1
+Insert	Into tbl_SEP_Reason Select	'In the last 12 months, I joined a Medicare Advantage plan with prescription drug coverage when I turned 65.', '12J', 1
+Insert	Into tbl_SEP_Reason Select	'Other', 'OTH', 1
+Insert	Into tbl_SEP_Reason Select	'I am currently receiving extra help paying for Medicare prescription drug coverage, but do not have Medicaid.', 'HLP', 1
+Insert	Into tbl_SEP_Reason Select	'I am no longer eligible for extra help paying for my Medicare prescription drugs.', 'NLS', 1
+Insert	Into tbl_SEP_Reason Select	'My plan is ending its contract with Medicare.', 'EOC', 1
+Insert	Into tbl_SEP_Reason Select	'I am disenrolling from a Medicare cost plan and had Medicare prescription drug coverage from the Medicare cost plan.', 'MCP', 1
+Insert	Into tbl_SEP_Reason Select	'I am being disenrolled from a Medicare special needs plan because I no longer have special needs status.', 'SNP', 1
+Insert	Into tbl_SEP_Reason Select	'I recently lost Medicare Part B but I still have Part A.', 'MEP', 1
+Insert	Into tbl_SEP_Reason Select	'I have had Medicare prior to now, but am now turning 65.', 'MRD', 1
+Insert	Into tbl_SEP_Reason Select	'I am enrolling in a 5-star Medicare plan.', '5ST', 1
+Insert	Into tbl_SEP_Reason Select	'I am currently enrolled in a Medicare Advantage Plan and would like to leave my plan to switch to Original Medicare and join a prescription drug plan.', 'JLY', 0
+Insert	Into tbl_SEP_Reason Select	'I am a Medicare beneficiary under 65 and have the opportunity to buy a Medigap policy in Georgia or Tennessee.', 'GMS', 0
+Insert	Into tbl_SEP_Reason Select	'I am eligible for the Environmental Health Hazards (EHH) Medicare Pilot Program.', 'EHH', 0
+Insert	Into tbl_SEP_Reason Select	'I am moving into or out of a Long Term Care Facility (for example, a nursing home or rehabilitation hospital).', 'LLT', 0
+Insert	Into tbl_SEP_Reason Select	'I live in a Hurricane Katrina Zip Code.', 'KAT', 0
+Insert	Into tbl_SEP_Reason Select	'I am in a Medicare Advantage plan with prescription drug coverage and am still in my 12 month trial period.', 'MAT', 0
+Insert	Into tbl_SEP_Reason Select	'I received a letter from my current plan indicating that I have an opportunity to make a Medicare drug plan selection until February 14, 2007.', 'PLA', 0
+Insert	Into tbl_SEP_Reason Select	'I am eligible to join or leave a Medicare Advantage Plan.', 'MAP', 0
+Insert	Into tbl_SEP_Reason Select	'I’m enrolled in the Original Medicare Plan.', 'OHM', 0
+Insert	Into tbl_SEP_Reason Select	'You received a notice from your plan or Medicare telling you that you are eligible for a Special Enrollment Period.', 'SLT', 0
